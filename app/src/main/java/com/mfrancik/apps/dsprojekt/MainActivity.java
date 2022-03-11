bdaeee519ab5cadefd2ce41ce9eb975d70ec7287
Frendzlu <mateuszfrancik.mtf@gmail.com>
2022-03-11 12:45:56 +0100

    obrazki

--- app/src/main/java/com/mfrancik/apps/dsprojekt/MainActivity.java
+++ app/src/main/java/com/mfrancik/apps/dsprojekt/MainActivity.java
@@ -3,12 +3,50 @@ package com.mfrancik.apps.dsprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button cameraButton = findViewById(R.id.cameraButton);
+    private Button albumsButton = findViewById(R.id.albumsButton);
+    private Button collageButton = findViewById(R.id.collageButton);
+    private Button networkingButton = findViewById(R.id.networkingButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
