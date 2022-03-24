1206c5b5b81fae320b20b21557e112ec8b2b8913
Frendzlu <mateuszfrancik.mtf@gmail.com>
2022-03-24 14:29:01 +0100

    comyeet

--- app/src/main/java/com/mfrancik/apps/dsprojekt/Albums.java
+++ app/src/main/java/com/mfrancik/apps/dsprojekt/Albums.java
@@ -1,14 +1,131 @@
 package com.mfrancik.apps.dsprojekt;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.HashMap;
 
 public class Albums extends AppCompatActivity {
+    public ListView albumsList;
+    public ImageView addButton;
+    private File pic;
+    private HashMap<String, File> fileMap;
+    private String[] fileMapKeys;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_albums);
+
+        albumsList = findViewById(R.id.albumsListView);
+        addButton = findViewById(R.id.addDirectoryButton);
+        ActionBar actionBar =  getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        fileMap = new HashMap<String, File>();
+
+        this.pic = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_PICTURES );
+        for (File file : pic.listFiles()){
+            fileMap.put(file.getName(), file);
+        }
+
+        createAdapter();
+
+        albumsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("TAG","numer klikanego wiersza w ListView = " + i);
+            }
+        });
+
+        albumsList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(Albums.this);
+                alert.setTitle("Usuń");
+                alert.setMessage("Czy na pewno chcesz usunąć ten folder?");
+                alert.setPositiveButton("Tak", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        File dir = fileMap.get(fileMapKeys[i]);
+                        for (File file : dir.listFiles()){
+                            file.delete();
+                        }
+                        dir.delete();
+                        fileMap.remove(fileMapKeys[i]);
+                        createAdapter();
+                    }
+                });
+                alert.setNegativeButton("Nie", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //wyświetl which
+                    }
+                });
+                alert.show();
+                return true;
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(Albums.this);
+                alert.setTitle("Nowy folder");
+                alert.setMessage("Podaj nazwę nowego folderu:");
+                EditText input = new EditText(Albums.this);
+                input.setText("nazwa folderu");
+                alert.setView(input);
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        File file = new File(pic, input.getText().toString());
+                        file.mkdir();
+                        fileMap.put(file.getName(), file);
+                        createAdapter();
+                    }
+                });
+                alert.setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //wyświetl which
+                    }
+                });
+                alert.show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createAdapter() {
+        fileMapKeys = fileMap.keySet().toArray(new String[0]);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                Albums.this,
+                R.layout.list_row,
+                R.id.albumName,
+                fileMapKeys
+        );
+        albumsList.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/mfrancik/apps/dsprojekt/MainActivity.java
+++ app/src/main/java/com/mfrancik/apps/dsprojekt/MainActivity.java
@@ -1,52 +1,98 @@
 package com.mfrancik.apps.dsprojekt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    private Button cameraButton = findViewById(R.id.cameraButton);
-    private Button albumsButton = findViewById(R.id.albumsButton);
-    private Button collageButton = findViewById(R.id.collageButton);
-    private Button networkingButton = findViewById(R.id.networkingButton);
+	private LinearLayout cameraButton;
+	private LinearLayout albumsButton;
+	private LinearLayout collageButton;
+	private LinearLayout networkingButton;
+	private boolean unlockGallery = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+		cameraButton = findViewById(R.id.cameraButton);
+		albumsButton = findViewById(R.id.albumsButton);
+		collageButton = findViewById(R.id.collageButton);
+		networkingButton = findViewById(R.id.networkingButton);
 
-        button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+		cameraButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, Camera.class);
+				startActivity(intent);
+			}
+		});
 
-            }
-        });
+		albumsButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 100);
+				if (unlockGallery) {
+					Intent intent = new Intent(MainActivity.this, Albums.class);
+					startActivity(intent);
+				}
+			}
+		});
 
-        button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+		collageButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, Collage.class);
+				startActivity(intent);
+			}
+		});
 
-            }
-        });
+		networkingButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, Network.class);
+				startActivity(intent);
+			}
+		});
+	}
 
-        button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+	public void checkPermission(String permission, int requestCode) {
+		if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+			ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+		} else {
+			this.unlockGallery = true;
+			Toast.makeText(MainActivity.this, String.format("Permission '%s' already granted", permission), Toast.LENGTH_SHORT).show();
+		}
+	}
 
-            }
-        });
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
-        button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 100 && grantResults.length > 0 &&
+				grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+			File pic = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_PICTURES );
 
-            }
-        });
-    }
+			new File(pic, "people").mkdir();
+			new File(pic, "places").mkdir();
+			new File(pic, "things").mkdir();
+
+			this.unlockGallery = true;
+		}
+	}
 }
