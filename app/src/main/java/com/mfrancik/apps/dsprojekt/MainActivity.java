a781a60c921ccbfc13155b63b9f0b322a919e852
Frendzlu <mateuszfrancik.mtf@gmail.com>
2022-04-29 11:48:13 +0200

    cleanup

--- app/src/main/java/com/mfrancik/apps/dsprojekt/MainActivity.java
+++ app/src/main/java/com/mfrancik/apps/dsprojekt/MainActivity.java
@@ -1,15 +1,7 @@
 package com.mfrancik.apps.dsprojekt;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -18,14 +10,18 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -33,7 +29,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -41,98 +36,88 @@ import java.util.Objects;
 import java.util.function.Consumer;
 
 public class MainActivity extends AppCompatActivity {
-	private LinearLayout cameraButton;
-	private LinearLayout albumsButton;
-	private LinearLayout collageButton;
-	private LinearLayout networkingButton;
-	private HashMap<Integer, Consumer<Integer>> afterPermissionGranted = new HashMap<Integer, Consumer<Integer>>(){};
+	private final HashMap<Integer, Consumer<Integer>> afterPermissionGranted = new HashMap<Integer, Consumer<Integer>>(){};
 	private String filePath;
 
+	@RequiresApi(api = Build.VERSION_CODES.N)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Log.d("DIS-TAG", "onCreate: aaaaaaa");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		cameraButton = findViewById(R.id.cameraButton);
-		albumsButton = findViewById(R.id.albumsButton);
-		collageButton = findViewById(R.id.collageButton);
-		networkingButton = findViewById(R.id.networkingButton);
+		LinearLayout cameraButton = findViewById(R.id.cameraButton);
+		LinearLayout albumsButton = findViewById(R.id.albumsButton);
+		LinearLayout collageButton = findViewById(R.id.collageButton);
+		LinearLayout networkingButton = findViewById(R.id.networkingButton);
 		File pic = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_PICTURES );
 
 		File x = new File(pic, "MateuszFrancik");
 		x.mkdir();
 		this.filePath = x.getPath();
-		cameraButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
-				alert.setTitle("Photo source");
-				alert.setItems(new String[]{"Camera", "Gallery"}, new DialogInterface.OnClickListener() {
-
-					@RequiresApi(api = Build.VERSION_CODES.N)
-					public void onClick(DialogInterface dialog, int which) {
-						switch (which) {
-							case 0:
-								try {
-									intentWithPermission(Manifest.permission.CAMERA, 101, (a) -> {
-										Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-										if (intent.resolveActivity(getPackageManager()) != null) {
-											startActivityForResult(intent, 200);
-										}
-									});
-								} catch (InvocationTargetException | IllegalAccessException e) {
-									e.printStackTrace();
+		cameraButton.setOnClickListener(v -> {
+			AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+			alert.setTitle("Photo source");
+			alert.setItems(new String[]{"Camera", "Gallery"}, (dialog, which) -> {
+				switch (which) {
+					case 0:
+						try {
+							intentWithPermission(Manifest.permission.CAMERA, 101, (a) -> {
+								Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+								if (intent.resolveActivity(getPackageManager()) != null) {
+									startActivityForResult(intent, 200);
 								}
-								break;
-							case 1:
-								if (checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 100)) {
-									intent[0] = new Intent(Intent.ACTION_PICK);
-									intent[0].setType("image/*");
-									startActivityForResult(intent[0], 201);
-								}
-								break;
+							});
+						} catch (InvocationTargetException | IllegalAccessException e) {
+							e.printStackTrace();
 						}
-					}
+						break;
+					case 1:
+						try {
+							intentWithPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 100, (e) -> {
+								Intent intent = new Intent(Intent.ACTION_PICK);
+								intent.setType("image/*");
+								startActivityForResult(intent, 201);
+							});
+						} catch (InvocationTargetException | IllegalAccessException e) {
+							e.printStackTrace();
+						}
+						break;
+				}
+			});
+			alert.show();
+		});
+
+		albumsButton.setOnClickListener(v -> {
+			try {
+				intentWithPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 100, (a) -> {
+					Intent intent = new Intent(MainActivity.this, Albums.class);
+					startActivity(intent);
 				});
-				alert.show();
+			} catch (InvocationTargetException | IllegalAccessException e) {
+				e.printStackTrace();
 			}
 		});
 
-		albumsButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 100)) {
+		albumsButton.setOnClickListener(v -> {
+			try {
+				intentWithPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 100, (e) -> {
 					Intent intent = new Intent(MainActivity.this, Albums.class);
 					startActivity(intent);
-				}
+				});
+			} catch (InvocationTargetException | IllegalAccessException e) {
+				e.printStackTrace();
 			}
 		});
 
-		albumsButton.setOnClickListener( {
-			@Override
-			public void onClick(View v) {
-				if (checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 100)) {
-					Intent intent = new Intent(MainActivity.this, Albums.class);
-					startActivity(intent);
-				}
-			}
+		collageButton.setOnClickListener(v -> {
+			Intent intent = new Intent(MainActivity.this, Collage.class);
+			startActivity(intent);
 		});
 
-		collageButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, Collage.class);
-				startActivity(intent);
-			}
-		});
-
-		networkingButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, Network.class);
-				startActivity(intent);
-			}
+		networkingButton.setOnClickListener(v -> {
+			Intent intent = new Intent(MainActivity.this, Network.class);
+			startActivity(intent);
 		});
 	}
 
@@ -143,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
 			ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
 		} else {
 			Toast.makeText(MainActivity.this, String.format("Permission '%s' already granted", permission), Toast.LENGTH_SHORT).show();
-			afterPermissionGranted.get(requestCode).accept(0);
+			Objects.requireNonNull(afterPermissionGranted.get(requestCode)).accept(0);
 		}
 	}
 
@@ -164,10 +149,10 @@ public class MainActivity extends AppCompatActivity {
 					new File(x, "places").mkdir();
 					new File(x, "things").mkdir();
 
-					afterPermissionGranted.get(requestCode).accept(0);
+					Objects.requireNonNull(afterPermissionGranted.get(requestCode)).accept(0);
 					break;
 				case 101:
-					afterPermissionGranted.get(requestCode).accept(0);
+					Objects.requireNonNull(afterPermissionGranted.get(requestCode)).accept(0);
 					break;
 			}
 		}
@@ -180,12 +165,13 @@ public class MainActivity extends AppCompatActivity {
 			case 200:
 				if (resultCode == RESULT_OK) {
 					ByteArrayOutputStream stream = new ByteArrayOutputStream();
+					assert data != null;
 					Bundle extras = data.getExtras();
 					Bitmap bitmappe = (Bitmap) extras.get("data");
 					bitmappe.compress(Bitmap.CompressFormat.JPEG, 100, stream);
 					byte[] byteArray = stream.toByteArray();
 
-					FileOutputStream fs = null;
+					FileOutputStream fs;
 					try {
 						SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd_HHmmss");
 						String d = df.format(new Date());
@@ -200,6 +186,7 @@ public class MainActivity extends AppCompatActivity {
 				break;
 			case 201:
 				if  (resultCode == RESULT_OK) {
+					assert data != null;
 					Uri imgData = data.getData();
 					ByteArrayOutputStream streamme = new ByteArrayOutputStream();
 					InputStream stream = null;
@@ -213,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
 					b.compress(Bitmap.CompressFormat.JPEG, 100, streamme); // kompresja, typ pliku jpg, png
 					byte[] byteArray = streamme.toByteArray();
 					Log.d("DIS-TAG", "Got here 2");
-					FileOutputStream fs = null;
+					FileOutputStream fs;
 					try {
 						SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd_HHmmss");
 						String d = df.format(new Date());
